ad884774c0ac4ed5c798f47a014def641ff4a9f3
Zhenzh567 <zhenzhg@yandex.ru>
2026-01-25 19:54:16 +0200

    Update main.cpp

--- lab/main.cpp
+++ lab/main.cpp
@@ -77,6 +77,9 @@ public:
     double getExpiration_date() const {
         return expiration_date;
     }
+    std::string getDesription() const {
+        return description;
+    }
     //сеттер на цену
     void setPrice(double newPrice) {
         if (newPrice >= 0) {
@@ -98,7 +101,7 @@ public:
     void locationInfo() {
         std::cout << location << std::endl;
     }
-    void setExpirationDate(double newExpirationDate) {
+    void decreaseExpirationDate(double newExpirationDate) {
         if (newExpirationDate < 0) {
             std::cout << "Ошибка: дата не может быть отрицательной" << std::endl;
             return;
@@ -119,10 +122,47 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "RUSSIAN");
+    //тестирование конструктора полного заполнения
     Product product1("Молоко","Топлённое",80,8,"склад1");
-    product1.setPrice(100);
-    product1.setExpirationDate(9);
     product1.displayInfo();
+    Product product2("Молоко","Топлённое",-80,8,"склад1");
+    product2.displayInfo();
+    Product product3("Молоко","Топлённое",80,-8,"склад1");
+    product3.displayInfo();
+    //тестирование конструктора по умолчанию
+    Product product4;
+    product2.displayInfo();
+    //тестирование контсруктора копирования
+    Product product5(product1);
+    std::cout<<"копия product1"<<std::endl;
+    product5.displayInfo();
+    //тестирование геттеров
+    std::cout<<product1.getName()<<std::endl;
+    std::cout<<product1.getPrice()<<std::endl;
+    std::cout<<product1.getDesription()<<std::endl;
+    std::cout<<product1.getExpiration_date()<<std::endl;
+    std::cout<<product1.getLocation()<<std::endl;
+    //тестирование сеттера на цену
+    std::cout<<"Текущая цена product1"<<product1.getPrice()<<std::endl;
+    product1.setPrice(100);//корректное значение
+    std::cout << "после изменения цены" << product1.getPrice()<<std::endl;
+    product1.setPrice(-50);//неккоректное значение
+    std::cout<<"после неккоректного изменения цены"<<product1.getPrice()<<std::endl;
+    //тестирование уменьшения срока годности
+    std::cout<<"Текущий срок годности product1"<<product1.getExpiration_date()<<std::endl;
+    product1.decreaseExpirationDate(5);//корректное значение
+    std::cout<<"после корректного изменения срока годности"<<product1.getExpiration_date()<<std::endl;
+    product1.decreaseExpirationDate(10);//неккоректное значение(больше предыдущего)
+    std::cout<<"после некорректного изменения срока годности"<<product1.getExpiration_date()<<std::endl;
+    product1.decreaseExpirationDate(-10);// неккоректное значение(отрицательное)
+    std::cout<<"после некорректного изменения срока годности"<<product1.getExpiration_date()<<std::endl;
+    //тестирование locationInfo()
+    product1.locationInfo();
+    product2.locationInfo();
+    product3.locationInfo();
+    product4.locationInfo();
+    product5.locationInfo();
+
 
     return 0;
 }
